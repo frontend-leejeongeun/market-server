01d5b32b7747f3cc895ddcefe711deadd53fc0e1
frontenddeveloper-leejeongeun <49803637+frontend-leejeongeun@users.noreply.github.com>
2024-10-16 13:15:35 +0900

    마켓 서버 시작

---
+++ index.js
@@ -0,0 +1,21 @@
+const server = http.createServer(function (req, res) {
+  const path = req.url;
+  const method = req.method;
+  if (path === "/products") {
+    if (method === "GET") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      const products = JSON.stringify([
+        {
+          name: "토마토",
+          price: 5000,
+        },
+      ]);
+      res.end(products);
+    } else if (method === "POST") {
+      res.end("생성되었습니다!");
+    }
+  }
+  res.end("Good Bye");
+});
+server.listen(port, hostname);
+console.log("grab market server on!");
